2658fd5a56677a438c860a7efd1bac446584005f
Surya <suryaprasathram@gmail.com>
2021-09-22 19:45:49 +0530

    vtol animation script added along with two rk4 scripts, updated params

--- param.py
+++ param.py
@@ -1,3 +1,5 @@
+from signal_gen import SignalGenerator
+import numpy as np
 
 # mass specs
 m_c = 1.53
@@ -7,5 +9,41 @@ m_u = 0.121
 # inertia specs
 J_c = 0.0049
 
-# geometric specs
-d = 0.38
+# body-rotor bridge dimension
+d_br = 0.38
+h_br = 0.05
+
+# body dimensions
+l_b = 0.2
+w_b = 0.2
+
+# rotor dimensions
+w_r = w_l = 0.25
+h_r = h_l = w_b
+
+# target dimensions
+l_t = 0.2
+w_t = 0.2
+
+# initial conditions
+z_i = 0
+h_i = w_b/2
+theta_i = 0
+target_i = -l_t/2
+zv_i = 0
+hv_i = 0
+thetav_i = 0
+targetv_i = 0
+
+# ground and sky dimension
+l_g = 3
+l_s = 5
+
+# coordinate functions
+z =  SignalGenerator().sin
+h = SignalGenerator().sin
+theta = SignalGenerator().sin
+
+dr_mat_i = np.array([[-d_br, h_br/2], [-l_b/2, h_br/2], [-l_b/2, w_b/2], [0, 0], [l_b/2, w_b/2], 
+          [l_b/2, h_br/2], [d_br, h_br/2], [d_br, -h_br/2], [l_b/2, -h_br/2], [l_b/2, -w_b/2], [-l_b/2, -w_b/2], 
+          [-l_b/2, -h_br/2], [-d_br, -h_br/2], [-d_br, h_br/2]])
---
+++ vtolAnimation.py
@@ -0,0 +1,63 @@
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import numpy as np
+import param as p
+
+# plt.ion()
+
+class vtolAnimation:
+
+    def __init__(self) -> None:
+        
+        self.first = self.first_t = True
+        self.fig, self.ax = plt.subplots()
+        self.handle = dict()
+
+        self.ax.plot([-2, 2], [0.00,0.00], color = 'black')      
+        self.ax.set_xlim(-2, 2)
+        self.ax.set_ylim(-0.5, 3.5)
+
+    
+    def update(self, z, h, theta, target = 0) -> None:
+
+        # updating the drone:
+
+        # under rotation of angle q:
+        # (a,b) -> (acosq - bsinq, asinq + bcosq)
+        
+        # the rotation matrix involved
+        # [[cosq, -sinq]
+        #  [sinq, cosq]]
+
+        rot_mat = np.array([[np.cos(theta), -np.sin(theta)],
+                 [np.sin(theta), np.cos(theta)]])
+        stats = np.matmul(rot_mat, p.dr_mat_i.T).T
+
+        # translation by z and h
+        stats = np.add(stats, np.array([z, h])) 
+
+        # update animation
+        self.draw_drone(stats)
+        if target != 0:
+            self.draw_target(target)
+
+
+    def draw_drone(self, stats) -> None:
+        
+        if self.first == True:
+            drone = mpatches.Polygon(np.add(p.dr_mat_i, [p.z_i, p.h_i]), closed = True)
+            self.handle['drone_body'] = drone
+            self.ax.add_patch(drone)
+            self.first = False
+        else:
+            self.handle['drone_body'].set_xy(stats)
+
+    def draw_target(self, x) -> None:
+       
+        if self.first_t == True:
+            target_body = mpatches.Rectangle([p.target_i,0], p.l_t, p.w_t, edgecolor = 'red', alpha = 0.8)
+            self.handle['target'] = target_body
+            self.ax.add_patch(self.handle['target'])
+            self.first_t = False
+        else:
+            self.handle['target'].set_x(x - p.l_t/2)
