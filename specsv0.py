cba68fce1723992d4886b6251af42dfa0206e394
Surya <suryaprasathram@gmail.com>
2021-11-18 00:32:44 +0530

    Final Shot
    Not going for anymore modifications.
    This will continue in vtol-v2

--- specsv0.py
+++ specsv0.py
@@ -1,25 +1,17 @@
 import random
 import numpy as np
-from find_best_tr import return_ks
 
 #################################### DO NOT CHANGE !!!!! ##################################################
-u_param = 0.2
-
-# _m_c = 1.53*(1 + u_param*random.randint(0, 10000)/10000)
-# _J_c = 0.0049*(1 + u_param*random.randint(0, 10000)/10000)
-# _d_br = 0.38*(1 + u_param*random.randint(0, 10000)/10000)
-# _m_u = 0.121*(1 + u_param*random.randint(0, 10000)/10000)
-
-_m_c = 1.53
-_J_c = 0.0049
-_d_br = 0.38
-_m_u = 0.121
-
 
 # mass specs
+_m_c = 1.53
 _m_r = _m_l = 0.22
 
+# inertia specs
+_J_c = 0.0049
+
 # body-rotor bridge dimension
+_d_br = 0.38
 _h_br = 0.05
 
 # body dimensions
@@ -46,6 +38,8 @@ def _u_c(t):
     """returns with coeffecients for Tau"""
     return np.array([-np.sin(t)/(_m_c + _m_l + _m_r), np.cos(t)/(_m_c + _m_l + _m_r), _d_br/(_J_c + (_m_l + _m_r)*(_d_br**2))])
 
+# damping specs
+_m_u = 0.121
 
 #################################### DO NOT CHANGE !!!!! ##################################################
 
@@ -90,7 +84,6 @@ kdc_theta = 1.932
 # max_values:
 max_force = 20
 max_torque = 20
-max_theta = 90*np.pi/180
 
 # saturating motors
 def saturate_motors(a):
@@ -104,16 +97,6 @@ def saturate_motors(a):
     return a
 
 def uncertainity(a, u_param = 0.2):
-    # for i in range(len(a)):
-        # a[i] = a[i]*
+    for i in range(len(a)):
+        a[i] = a[i]*(1 + u_param*random.randint(0, 10000)/10000)
     return a
-
-# specs after fastest rise time
-_o_kd_alt, _o_kp_alt, _o_kdc_lat, _o_kd_lat, _o_kp_lat = return_ks(10.47, 10.47)
-
-_ell = 0.707
-
-_o_kd_theta, _o_kp_theta = (2*0.707*2.2/1.047)*0.068436, ((2.2/1.047)**2)*0.068436 
-_o_ki_lat = 0.0001
-_o_ki_alt = 0
-
